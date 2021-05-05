0052118914cf03cd2b3557111116e2e074a15117
adreansyahasw <adreansyah.aswafi@sg-dsa.com>
2021-05-05 14:47:13 +0700

    patch html creators node API

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const movieRouter = require('./routes/movie-router');
 const credentials = require('./routes/users-credential-router');
 const menus = require('./routes/menu-router');
 const app = express();
-const apiPort =12000;
+const apiPort = 12000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('index.html', { root: __dirname + '/public' })
 });
 app.use('/api', authRouter);
 app.use('/api', movieRouter);
